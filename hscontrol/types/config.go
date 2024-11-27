d627292f18263553322828aac7285e6f73dfba53
seiuneko <seiunekosl@gmail.com>
2024-11-27 16:11:15 +0800

    feat: add verify client config for embedded DERP

--- hscontrol/types/config.go
+++ hscontrol/types/config.go
@@ -177,6 +177,8 @@ type DERPConfig struct {
 	ServerRegionCode                   string
 	ServerRegionName                   string
 	ServerPrivateKeyPath               string
+	ServerVerifyClientURL              string
+	ServerVerifyFailOpen               bool
 	STUNAddr                           string
 	URLs                               []url.URL
 	Paths                              []string
@@ -417,6 +419,8 @@ func derpConfig() DERPConfig {
 	serverRegionID := viper.GetInt("derp.server.region_id")
 	serverRegionCode := viper.GetString("derp.server.region_code")
 	serverRegionName := viper.GetString("derp.server.region_name")
+	serverVerifyClientURL := viper.GetString("derp.server.verify_client_url")
+	serverVerifyFailOpen := viper.GetBool("derp.server.verify_client_url_fail_open")
 	stunAddr := viper.GetString("derp.server.stun_listen_addr")
 	privateKeyPath := util.AbsolutePathFromConfigPath(
 		viper.GetString("derp.server.private_key_path"),
@@ -461,6 +465,8 @@ func derpConfig() DERPConfig {
 		ServerRegionID:                     serverRegionID,
 		ServerRegionCode:                   serverRegionCode,
 		ServerRegionName:                   serverRegionName,
+		ServerVerifyClientURL:              serverVerifyClientURL,
+		ServerVerifyFailOpen:               serverVerifyFailOpen,
 		ServerPrivateKeyPath:               privateKeyPath,
 		STUNAddr:                           stunAddr,
 		URLs:                               urls,
